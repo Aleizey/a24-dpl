871af1bdf1a455b4165bc58c3f719f439e366070
Alexloven <reyvaldiviaalejandro@gmail.com>
2025-02-06 21:10:53 +0100

    cors localhost edit

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(cors({
-  origin: ['https://aleizey.github.io','http://localhost:8000'],
+  origin: ['https://aleizey.github.io','http://localhost:8080'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ' Content-Type, Authorization',
 }))
